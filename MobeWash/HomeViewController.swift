58146748c20d358a912ffce2dd12549a2ed4caec
Shuangli Zhou <shz103@ucsd.edu>
2017-04-19 11:41:21 -0700

    New Launch Screen Added
    Changed Launch Screen. Linked UIViewController, added module attribute.

--- MobeWash/HomeViewController.swift
+++ MobeWash/HomeViewController.swift
@@ -13,6 +13,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSLog("For testing purpose only.")
     }
 
     override func didReceiveMemoryWarning() {
