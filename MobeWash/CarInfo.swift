85265bf77d0b4c249d043cb061aff8d4daff4b84
Zitong Zhang <ziz098@ucsd.edu>
2017-04-22 14:31:33 -0700

    classes

---
+++ MobeWash/CarInfo.swift
@@ -0,0 +1,23 @@
+////
+////  CarInfo.swift
+////  MobeWash
+////
+////  Created by 张子桐 on 4/21/17.
+////
+////
+//
+//import Foundation
+//class CarInfo{
+//    //field
+//    private var _workTitle:String!
+//    private var _workTime:String!
+//    private var _workLocation:String!
+//    private var _carInfo:CarInfo!
+//    private var _package:String!
+//    
+//    //init
+//    
+//    init(workTitle) {
+//        <#statements#>
+//    }
+//}
