ca8f22d94f05bc0946885bb3eaee71eca6388f4b
Shuangli Zhou <shz103@ucsd.edu>
2017-04-27 22:04:54 -0700

    TableView Delegation & Datasource added

--- MobeWash/Ready2BookViewController.swift
+++ MobeWash/Ready2BookViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class Ready2BookViewController: UIViewController, UITextFieldDelegate {
+class Ready2BookViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var companyInput: UITextField!
-
+    @IBOutlet weak var companyTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // Leon - set up new delegation method
         companyInput.delegate = self
     }
 
@@ -28,5 +30,18 @@ class Ready2BookViewController: UIViewController, UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+   
 }
