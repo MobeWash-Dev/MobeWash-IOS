d54d196e60602eb0e1481f58d740e0079e8a29fc
Shuangli Zhou <shz103@ucsd.edu>
2017-04-27 21:57:30 -0700

    Added textfield delegation
    Added textfield delegation, return key now functional.

--- MobeWash/Ready2BookViewController.swift
+++ MobeWash/Ready2BookViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class Ready2BookViewController: UIViewController {
+class Ready2BookViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var companyInput: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        companyInput.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +24,9 @@ class Ready2BookViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
-    */
 
 }
