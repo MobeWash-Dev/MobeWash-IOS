266c1d901b3294330b768a25cfaf4323b3c8d0a5
Zhizhen Qin <EliQinZZ1997@gmail.com>
2017-04-30 01:17:49 -0700

    Finished with testing

---
+++ MobeWash/WasherHistoryTableViewController.swift
@@ -0,0 +1,104 @@
+//
+//  WasherHistoryTableViewController.swift
+//  MobeWash
+//
+//  Created by Z.Z. on 30/04/2017.
+//
+//
+
+import UIKit
+
+class WasherHistoryTableViewController: UITableViewController {
+    
+    var packages:[String] = ["Mobe", "MobePlus", "Mobe"]
+    var carType:[String]  = ["Lamborghini Gallardo", "Jeep Grand Cherokee", "McLaren P1"]
+    var time:[String] = ["Jan 25th, 15:00", "Jan 26th, 10:00", "Jan 27th, 13:30"]
+    var carImages:[String] = ["LG.jpg", "JGC.jpg", "MP.jpg"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return carType.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "HistoryCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! WasherHistoryTableViewCell
+        
+        cell.packageLabel.text = packages[indexPath.row]
+        cell.carTypeLabel.text = carType[indexPath.row]
+        cell.dateLabel.text = time[indexPath.row]
+        cell.thumbnailImageView.image = UIImage(named: carImages[indexPath.row])
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
