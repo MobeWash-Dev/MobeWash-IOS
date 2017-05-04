d10687ef58e58c65de4601e5c2c122a95601c016
Kat He <jiaweihe96@hotmail.com>
2017-05-04 16:38:33 -0700

    Add Time Slot Tableview logic and set up to work with calendar

--- MobeWash/TimeViewController.swift
+++ MobeWash/TimeViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import FSCalendar
 
-class TimeViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance {
+class TimeViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance, UITableViewDataSource, UITableViewDelegate {
     
     // Dummy Data
     @IBOutlet weak var calendar: FSCalendar!
@@ -40,6 +40,8 @@ class TimeViewController: UIViewController, FSCalendarDataSource, FSCalendarDele
     var datesWithAvailableWashers = ["2017/05/03", "2017/05/04", "2017/05/07"]
     var datesWithUnAvailableWashers = ["2017/05/05", "2017/05/06", "2017/05/08"]
     
+    var timeTableView = UITableView()
+    var noTimesLabel = UILabel()
     
     override func loadView() {
         let view = UIView(frame: UIScreen.main.bounds)
@@ -58,13 +60,39 @@ class TimeViewController: UIViewController, FSCalendarDataSource, FSCalendarDele
         self.calendar = calendar
         
         calendar.select(Date())
-        //calendar.select(self.dateFormatter1.date(from: "2017/05/02"))
+        self.calendar.today = nil
         /*let todayItem = UIBarButtonItem(title: "TODAY", style: .plain, target: self, action: #selector(self.todayItemClicked(sender:)))
         self.navigationItem.rightBarButtonItem = todayItem*/
         
         // For UITest
         self.calendar.accessibilityIdentifier = "calendar"
-        self.calendar.today = nil
+        
+        
+        // Time Slot Tableview
+        self.setDateVariables(selectedDateAndTime: Date())
+        
+        timeTableView = UITableView(frame: CGRect(x:0, y:400, width: UIScreen.main.bounds.width, height: 200))
+        timeTableView.delegate = self
+        timeTableView.dataSource = self
+        timeTableView.register(UITableViewCell.self, forCellReuseIdentifier: "timeCell")
+        timeTableView.cellLayoutMarginsFollowReadableWidth = false
+        timeTableView.tableFooterView = UIView(frame: CGRect.zero)
+        self.view.addSubview(timeTableView)
+        
+        noTimesLabel = UILabel(frame: CGRect(x: 0, y: 300, width: 200, height: 21))
+        noTimesLabel.center = timeTableView.center
+        noTimesLabel.textAlignment = .center
+        noTimesLabel.text = "No Times Available"
+        noTimesLabel.font = noTimesLabel.font.withSize(20)
+        noTimesLabel.textColor = UIColor.gray
+        noTimesLabel.isHidden = true
+        self.view.addSubview(noTimesLabel)
+        
+        let tableHeader = UILabel(frame: CGRect(x:0, y:380, width: UIScreen.main.bounds.width, height: 20))
+        tableHeader.textAlignment = .center
+        tableHeader.text = "Time Slots Available"
+        tableHeader.backgroundColor = UIColor.lightGray
+        self.view.addSubview(tableHeader)
     }
     
     deinit {
@@ -101,6 +129,8 @@ class TimeViewController: UIViewController, FSCalendarDataSource, FSCalendarDele
             return false
         }
         
+        self.setDateVariables(selectedDateAndTime: date)
+        timeTableView.reloadData()
         return true
     }
     
@@ -125,6 +155,8 @@ class TimeViewController: UIViewController, FSCalendarDataSource, FSCalendarDele
         if let color = self.fillAvailableColors[key] {
             return color
         }
+        
+        
         return appearance.selectionColor
     }
     
@@ -134,6 +166,90 @@ class TimeViewController: UIViewController, FSCalendarDataSource, FSCalendarDele
             return color
         }
         return nil
+    }
+    
+    // Methods for Time Slot Tableview
+    // Dummy Data
+    let availability = ["May": [4: [10, 11, 12], 7: [8, 14]]]
+    
+    var noneAvailable = false
+    var selectedMonth = String()
+    var selectedDate = Int()
+    var displayDate = String()
+    var selectedTime = String()
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if noneAvailable == true{
+            return 0
+        }
+        
+        // Data source to be changed
+        return availability[selectedMonth]![selectedDate]!.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "timeCell")
+        
+        if noneAvailable == true{
+            return cell
+        }
+        
+        // Data source to be changed
+        let time = availability[selectedMonth]![selectedDate]![indexPath.row]
+        // Selecting AM and PM to be changed
+        if(time < 13){
+            cell.textLabel?.text = String(time)+"AM"
+        } else{
+            cell.textLabel?.text = String(time-12)+"PM"
+        }
+        
+        return cell
+    }
+    
+    internal func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if cell.responds(to: #selector(setter: UITableViewCell.separatorInset)) {
+            cell.separatorInset = UIEdgeInsets.zero
+        }
+        if cell.responds(to: #selector(setter: UIView.preservesSuperviewLayoutMargins)) {
+            cell.preservesSuperviewLayoutMargins = false
+        }
+        if cell.responds(to: #selector(setter: UIView.layoutMargins)) {
+            cell.layoutMargins = UIEdgeInsets.zero
+        }
+    }
+    
+    internal func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let currentCell = tableView.cellForRow(at: indexPath)
+        currentCell?.layer.borderColor = UIColor.gray.cgColor
+        selectedTime = (currentCell?.textLabel!.text)!
+    }
+    
+    func setDateVariables(selectedDateAndTime: Date){
+        let dateAndTime = selectedDateAndTime
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .long
+        
+        // ["Month Date", "Year", "Time"]
+        let dateTimeArray = formatter.string(from: dateAndTime).components(separatedBy: ",")
+        
+        // ["Month", "Date"]
+        let dateArray = dateTimeArray[0].components(separatedBy: " ")
+        
+        selectedMonth = dateArray[0]
+        selectedDate = Int(dateArray[1])!
+        displayDate = dateTimeArray[0]
+        
+        // Data Source to be changed
+        if availability[selectedMonth]?[selectedDate]?.isEmpty == nil{
+            noneAvailable = true
+            noTimesLabel.isHidden = false
+            //timeTableView.isHidden = true
+        } else{
+            noneAvailable = false
+            noTimesLabel.isHidden = true
+            //timeTableView.isHidden = false
+        }
     }
     
 //    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, borderDefaultColorFor date: Date) -> UIColor? {
