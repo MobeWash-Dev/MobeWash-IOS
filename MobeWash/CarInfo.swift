fe6ad71d4243ea9aa082a5a7e10b6d2df20be8b3
Zitong Zhang <ziz098@ucsd.edu>
2017-05-03 18:35:23 -0700

    done with detailed page

--- MobeWash/CarInfo.swift
+++ MobeWash/CarInfo.swift
@@ -9,19 +9,39 @@
 import Foundation
 class CarInfo{
     //field
-    private var _carMake:String
-    private var _carColor:String
-    private var _carPlate:String
-    private var _carModel:String
-    private var _carPicture:[String]
+    private var _carMake:String!
+    private var _carColor:String!
+    private var _carPlate:String!
+    private var _carModel:String!
+    private var _carPicture:String!
+    private var _ownerPhone:String!
     
     //init
     
-    init(carMake:String, carColor:String,carPlate:String,carModel:String,carPicture:[String] ) {
+    init(carMake:String, carColor:String,carPlate:String,carModel:String,carPicture:String,ownerPhone:String ) {
         self._carMake=carMake
         self._carColor = carColor
         self._carPlate=carPlate
         self._carModel=carModel
         self._carPicture=carPicture
+        self._ownerPhone=ownerPhone
+    }
+    var carMake:String{
+        return _carMake
+    }
+    var carColor:String!{
+        return _carColor
+    }
+    var carPlate:String{
+        return _carPlate
+    }
+    var carModel:String{
+        return _carModel
+    }
+    var carPicture:String{
+        return _carPicture
+    }
+    var ownerPhone:String{
+        return _ownerPhone
     }
 }
