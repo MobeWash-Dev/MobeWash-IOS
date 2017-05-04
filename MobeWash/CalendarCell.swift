e18ad411b092efdac62f05f718b7018e6b12f3df
Sean Ippolito <ippolitosean@gmail.com>
2017-05-04 16:44:43 -0700

    Calendar modifications and added table

--- MobeWash/CalendarCell.swift
+++ MobeWash/CalendarCell.swift
@@ -12,4 +12,13 @@ import JTAppleCalendar
 class CalendarCell: JTAppleCell {
 
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var strikeThrough: UILabel!
+
+    override var isSelected: Bool {
+        didSet {
+            self.layer.borderWidth = 3.0
+            self.layer.borderColor = isSelected ? UIColor.green.cgColor : UIColor.black.cgColor
+        }
+    }
+    
 }
--- MobeWash/CalendarViewController.swift
+++ MobeWash/CalendarViewController.swift
@@ -22,7 +22,7 @@ class CalendarViewController: UIViewController {
     }
 }
 
-extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+extension CalendarViewController: JTAppleCalendarViewDataSource {
     
     func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
         
@@ -32,17 +32,41 @@ extension CalendarViewController: JTAppleCalendarViewDelegate, JTAppleCalendarVi
         
         let start = formatter.date(from: "2017 01 01")!
         let end = formatter.date(from: "2017 12 31")!
+        calendar.allowsMultipleSelection = true
         
         let parameters = ConfigurationParameters(startDate: start, endDate: end)
         return parameters
     }
     
+    
+    
+}
+
+extension CalendarViewController: JTAppleCalendarViewDelegate {
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
         
+        
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: "-----")
+        attributeString.addAttribute(NSStrikethroughStyleAttributeName, value: 3, range: NSMakeRange(0, attributeString.length))
+        
+        let badDates : Set = ["5","9","10","11","20",]
+        
         cell.dateLabel.text = cellState.text
+        if cell.dateLabel.text == "12" || badDates.contains(cell.dateLabel.text!){
+            cell.strikeThrough.attributedText = attributeString
+        }
+        //cell.strikeThrough.attributedText = attributeString
+        
+        cell.layer.borderWidth = 2
+        cell.layer.borderColor = UIColor.black.cgColor
+        
+        if cell.isSelected {
+            cell.isHighlighted = false
+        }
+        
+        
         return cell
     }
-    
     
 }
