4a35583b05c8353eea18c285a034bac5b233514e
Kat He <jiaweihe96@hotmail.com>
2017-05-04 17:53:54 -0700

    Set up Time Slot Tableview for when calendar methods get implemented

--- MobeWash/CalendarViewController.swift
+++ MobeWash/CalendarViewController.swift
@@ -9,18 +9,114 @@
 import UIKit
 import JTAppleCalendar
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let formatter = DateFormatter()
-
+    
+    // Dummy Data
+    let availability = ["May": [4: [10, 11, 12], 7: [8, 14]]]
+    
+    var noneAvailable = false
+    var selectedMonth = String()
+    var selectedDate = Int()
+    var displayDate = String()
+    var selectedTime = String()
+    
+    
+    @IBOutlet weak var timeTableView: UITableView!
+    @IBOutlet weak var noTimesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setDateVariables(selectedDateAndTime: Date())
+        timeTableView.cellLayoutMarginsFollowReadableWidth = false
+        
+        timeTableView.tableFooterView = UIView(frame: CGRect.zero)
+        noTimesLabel.font = noTimesLabel.font.withSize(20)
+        noTimesLabel.textColor = UIColor.gray
+        noTimesLabel.isHidden = true
+    }
+    
+    func setDateVariables(selectedDateAndTime: Date){
+        let dateAndTime = selectedDateAndTime
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .long
+        
+        // ["Month Date", "Year", "Time"]
+        let dateTimeArray = formatter.string(from: dateAndTime).components(separatedBy: ",")
+        
+        // ["Month", "Date"]
+        let dateArray = dateTimeArray[0].components(separatedBy: " ")
+        
+        selectedMonth = dateArray[0]
+        selectedDate = Int(dateArray[1])!
+        displayDate = dateTimeArray[0]
+        
+        // Data Source to be changed
+        if availability[selectedMonth]?[selectedDate]?.isEmpty == nil{
+            noneAvailable = true
+            noTimesLabel.isHidden = false
+            timeTableView.isHidden = true
+        } else{
+            noneAvailable = false
+            noTimesLabel.isHidden = true
+            timeTableView.isHidden = false
+        }
+    }
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if noneAvailable == true{
+            return 0
+        }
+        
+        // Data source to be changed
+        return availability[selectedMonth]![selectedDate]!.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "timeCell")
+        
+        if noneAvailable == true{
+            return cell
+        }
+        
+        // Data source to be changed
+        let time = availability[selectedMonth]![selectedDate]![indexPath.row]
+        // Selecting AM and PM to be changed
+        if(time < 13){
+            cell.textLabel?.text = String(time)+"AM"
+        } else{
+            cell.textLabel?.text = String(time-12)+"PM"
+        }
+        
+        return cell
+    }
+    
+    internal func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if cell.responds(to: #selector(setter: UITableViewCell.separatorInset)) {
+            cell.separatorInset = UIEdgeInsets.zero
+        }
+        if cell.responds(to: #selector(setter: UIView.preservesSuperviewLayoutMargins)) {
+            cell.preservesSuperviewLayoutMargins = false
+        }
+        if cell.responds(to: #selector(setter: UIView.layoutMargins)) {
+            cell.layoutMargins = UIEdgeInsets.zero
+        }
+    }
+    
+    internal func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let currentCell = tableView.cellForRow(at: indexPath)
+        currentCell?.layer.borderColor = UIColor.gray.cgColor
+        selectedTime = (currentCell?.textLabel!.text)!
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 }
+
 
 extension CalendarViewController: JTAppleCalendarViewDataSource {
     
