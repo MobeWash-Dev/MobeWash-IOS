78616aa2e8713ed1839dee7ff588392b258c2d82
chadlohrli <chadlohrli@Chads-MacBook-Pro.local>
2017-05-28 21:20:43 -0700

    Fix

--- MobeWash/UserDetailsViewController.swift
+++ MobeWash/UserDetailsViewController.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2017 MobeWash. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class UserDetailsViewController: UIViewController, UITextFieldDelegate {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var textFields = [UITextField]()
+        textFields = [firstName, lastName, phoneNumber, emailField, makeAndModel,
+                      colorField, licenseField, extraInfo]
+        
+        for item in textFields{
+            item.borderStyle = .roundedRect
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
