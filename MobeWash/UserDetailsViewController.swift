75455a1009f329b879806908dcecf0c5a2bb6d93
chadlohrli <chadlohrli@Chads-MacBook-Pro.local>
2017-05-28 21:47:16 -0700

    Rounded TextFields

--- MobeWash/UserDetailsViewController.swift
+++ MobeWash/UserDetailsViewController.swift
@@ -11,10 +11,25 @@ import UIKit
 
 class UserDetailsViewController: UIViewController,UITextFieldDelegate {
     
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var makeField: UITextField!
+    @IBOutlet weak var colorField: UITextField!
+    @IBOutlet weak var licenseField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var infoField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        var textFields = [UITextField]()
+        textFields = [firstNameField, lastNameField, phoneField, emailField, makeField,
+                      colorField, licenseField, infoField]
+        
+        for item in textFields{
+            item.borderStyle = .roundedRect
+        }
     }
     
     override func didReceiveMemoryWarning() {
