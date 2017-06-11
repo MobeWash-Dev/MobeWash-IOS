191a37dd2e8a3767929df71799bf4c70e28a0dcd
Sean Ippolito <ippolitosean@gmail.com>
2017-06-10 21:34:16 -0700

    Revert "Revert "Merge pull request #38 from MobeWash-Dev/MW-IOS-50-Add-Card-Functionality-To-Billing-Page-""
    This reverts commit 3cea0def35b9e333d1110dbbffcf46ea25ba1ea2.

--- MobeWash/Buttons.swift
+++ MobeWash/Buttons.swift
@@ -30,7 +30,7 @@ class HighlightingButton: UIButton {
 
 class BuyButton : HighlightingButton {
     var disabledColor = UIColor.lightGray
-    var enabledColor = UIColor(red:0.22, green:0.65, blue:0.91, alpha:1.00)
+    var enabledColor = MWColors.MWBlue
     
 
     override var isEnabled: Bool {
@@ -47,8 +47,8 @@ class BuyButton : HighlightingButton {
         self.layer.borderWidth = 2
         self.layer.cornerRadius = 10
         self.setTitle("Book", for: UIControlState())
-        self.disabledColor = theme.secondaryForegroundColor
-        self.enabledColor = theme.accentColor
+        //self.disabledColor = theme.secondaryForegroundColor
+        //self.enabledColor = theme.accentColor
         self.isEnabled = enabled
     }
 }
@@ -71,6 +71,7 @@ class ApplePayBuyButton : HighlightingButton {
         self.layer.borderWidth = 2
         self.layer.cornerRadius = 10
         self.setTitle("Book with  Apple Pay", for: UIControlState())
+
         //self.disabledColor = theme.secondaryForegroundColor
         //self.enabledColor = theme.accentColor
         self.isEnabled = enabled
